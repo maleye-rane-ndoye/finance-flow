e02361581101a3edfb809a0a02dc1a236b5995c8
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-03 19:32:34 +0100

    AddBudget Component

---
+++ src/Components/AddBudget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Params from './Params'
+export default function AddBudget() {
+  return (
+    <>
+      <Params/>
+    <div className='AddBudget'>
+            this is the budget page list and formular add budget
+    </div>
+    </>
+  )
+}
