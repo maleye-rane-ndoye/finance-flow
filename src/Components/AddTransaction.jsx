be6b478ca8b83b4d7b492b843a25364acbb34b49
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-03 19:36:35 +0100

    AddTransaction Component

---
+++ src/Components/AddTransaction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Params from './Params'
+export default function AddTransaction() {
+  return (
+    <>
+        <Params/>
+        <div className='Transaction'>
+          this is the transaction components
+        </div>
+    </>
+  )
+}
