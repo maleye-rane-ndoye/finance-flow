54d4ac54770876a8393f85bc60dd71f8f9756667
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-03 19:38:59 +0100

    Stats Budget component

---
+++ src/Components/BudgetStats.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Params from './Params'
+export default function BudgetStats() {
+  return (
+    <>
+        <Params/>
+        <div className='BudgetStats'>
+            his is the budget stats
+        </div>
+    </>
+  )
+}
