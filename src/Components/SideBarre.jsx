262c6dda59d3c6eec4fbdbf78d256d8b190c2c84
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-04 10:20:56 +0100

    SideBarre component

---
+++ src/Components/SideBarre.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function SideBarre({active}) {
+
+   
+  return (
+    <aside className='sidebar' sidebare={active}>
+        <div className=' w-72 absolute flex flex-col items-center'>
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+           hello here the sidebar
+       </div>
+    </aside>
+  )
+}
