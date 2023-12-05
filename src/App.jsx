60d3785c0cbd0fde42254850a72e5e0256bc478e
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-05 18:20:25 +0100

    import budget and expense card to home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import { Routes,Route } from 'react-router-dom'
+import Home from './Components/Home'
+import NaveBare from './Components/NaveBare'
+import DeviceElements from './Components/DeviceElements'
+import AddBudget from './Components/AddBudget'
+import AddTransaction from './Components/AddTransaction'
+import BudgetStats from './Components/BudgetStats'
+import Profile from './Components/Profile'
 import './App.css'
-
-
-
-
-
-
-
-
-
-
-
 
 
 function App() {
  
 
   return (
-    <>
-      <p className=' text-yellow-300'>Maleye is here</p>
-    </>
+    <div className='App'>
+        <DeviceElements/>
+           <Routes><Route path='/' element = {<Home/>}></Route></Routes>
+           <Routes><Route path='/addBudget' element = {<AddBudget/>}></Route></Routes>
+           <Routes><Route path='/addTransaction' element = {<AddTransaction/>}></Route></Routes>
+           <Routes><Route path='/budgetStats' element = {<BudgetStats/>}></Route></Routes>
+           <Routes><Route path='/Profile' element = {<Profile/>}></Route></Routes>
+
+
+        <NaveBare/>
+    </div>
   )
 }
 
