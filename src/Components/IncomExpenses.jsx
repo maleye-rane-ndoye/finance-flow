44aedf4bef6c4534f3d3257944558954b6222938
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-08 12:37:12 +0100

    income and expenses show balance

---
+++ src/Components/IncomExpenses.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function IncomExpenses() {
+  return (
+    <div className='IncomExpenses'>
+       <div className='flex flex-row '>
+            <div className="Total drop-shadow-xl">
+              <span class="material-symbols-rounded text-blue-600">add_box</span>
+              <div className=' text-start'>
+                  <span className='hover:text-blue-500'>$1200</span>
+                  <p className=' text-xs truncate font-light'>Total income</p>
+              </div>
+            </div>
+
+            <div className="Expense drop-shadow-xl">
+              <span class="material-symbols-rounded text-red-600">do_not_disturb_on</span>
+              <div className=' text-start'>
+                  <span className='hover:text-blue-500'>$300</span>
+                  <p className=' text-xs truncate font-light'>Expenses</p>
+              </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
