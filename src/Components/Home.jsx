463477a5ef121cc2f73d1b119314edc0c812ae30
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-18 22:36:36 +0100

    delete button component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Params from './Params'
 import TransactionList from './TransactionList'
 import IncomExpenses from './IncomExpenses'
-import AddTransaction from './AddTransaction'
 export default function Home() {
   return (
     <>
@@ -10,7 +9,6 @@ export default function Home() {
       <div className='Home'>
        <IncomExpenses/>
        <TransactionList/>
-       <AddTransaction/>
       </div>
     </>
   )
