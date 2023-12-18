5d6109ce35d5f1356a5a8c92154bd72e1c6dc09e
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-18 22:22:46 +0100

    root component rendered

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AddTransaction from './Components/AddTransaction'
 import BudgetStats from './Components/BudgetStats'
 import Profile from './Components/Profile'
 import TransactionList from './Components/TransactionList'
+import AddBudget from './Components/AddBudget'
 import './App.css'
 
 
@@ -16,7 +17,7 @@ function App() {
     <div className='App'>
         <DeviceElements/>
            <Routes><Route path='/' element = {<Home/>}></Route></Routes>
-           <Routes><Route path='/TransactionList' element = {<TransactionList/>}></Route></Routes>
+           <Routes><Route path='/addBudget' element = {<AddBudget/>}></Route></Routes>
            <Routes><Route path='/addTransaction' element = {<AddTransaction/>}></Route></Routes>
            <Routes><Route path='/budgetStats' element = {<BudgetStats/>}></Route></Routes>
            <Routes><Route path='/Profile' element = {<Profile/>}></Route></Routes>
