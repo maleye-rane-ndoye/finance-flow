dec6386c988a229c7b5e57822607b1fc143bbaa8
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-18 22:44:54 +0100

    Profile deta

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,26 +1,46 @@
 import React from 'react'
 
+  const profile = [
+    {id:1, name:"General",},
+    {id:2, name:"Informations",},
+    {id:3, name:"Notifications",},
+    {id:4, name:"Privacy",},
+    {id:5, name:"files",},
+  ,
+
+    
+
+
+
+  ]
+
+
 export default function Profile() {
   return (
     <div className='Profile'>
         <div className='photo drop-shadow-2xl'>
+            <h2 className='text-lg font-semibold text-slate-50'>Your Profile</h2>
             <div className='photoContente'></div>
-            <h2 className=' text-xl font-bold'>Maleye</h2>
+            <h2 className=' text-xl font-bold text-slate-100'>Maleye</h2>
+
+            <span className=' text-slate-100 font-light'>Free plan</span>
         </div>
-        <div className='description drop-shadow-2xl'>
-           Maleye Rane Developper web et mobile <br />
-           etudiant à la l'universiter numerique la plateforme
-        </div>
+       
+
         <div className='details drop-shadow-2xl'>
-        <h2>Edite the profile</h2>
-            <div className='flex flex-col text-start  space-x-6'>
-                 <p>Prenom</p>
-                 <p>Nom</p>
-                 <p>Adresse</p>
-                 <p>Telephone</p>
-                 <p>email</p>
-                 <p></p>
-            </div>
+              {profile.map((mapItem) => {
+
+                  return(
+
+                        <div className='boxProfile  hover:drop-shadow-2xl' key={mapItem.id}>
+                            <p>{mapItem.name}</p>
+                            <span class="material-symbols-rounded hover:text-blue-300 text-lg">chevron_right</span>
+                        </div>
+
+                  );
+
+              })}
+            
         </div>
     </div>
   )
