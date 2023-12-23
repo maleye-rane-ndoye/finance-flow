59d6adfe0fd1353e8891f216a8c75cac9d1ad705
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-23 01:21:18 +0100

    TransactionList that mapping on the Transaction component

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { GlobalContext } from '../Context/GlobalStats'
 import { useContext } from 'react'
-import {Transaction} from './Transaction'
+import Transaction from './Transaction'
 
 export default function TransactionList() {
 
-  const { transactions } = useContext(GlobalContext)
+  const { transactions, getBudgetById, deleteTransaction } = useContext(GlobalContext)
 
   return (
     <ul>
-      {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/>))}
+      {transactions.map(transaction => (
+        <Transaction 
+          key={transaction.id} 
+          transaction={transaction} 
+          budget={getBudgetById(transaction.budgetId)}
+          onDeleteButtonClick={() => deleteTransaction(transaction.id)}
+        />
+      ))}
           
     </ul>
    
