f7af2ffc57cf3604facbf4ff9b40eb29e3307172
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-23 01:20:13 +0100

    Transaction main component

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -1,30 +1,50 @@
 import React from 'react'
-import { GlobalContext } from '../Context/GlobalStats';
-import { useContext } from 'react';
 
-export const Transaction = ({transaction}) => {
+
+const Transaction = (props) => {
+
+    const {
+        className,
+        transaction,
+        budget,
+        closeButtonHidden,
+        onDeleteButtonClick,
+
+    } = props;
+
+
 
     const sign = transaction.amount < 0 ? '-': '+';
     const color = transaction.amount < 0 ? 'material-symbols-rounded text-red-600' : 'material-symbols-rounded text-blue-600'
-    const trash = "material-symbols-rounded cursor-pointer text-slate-500"
+    const trash = "material-symbols-rounded cursor-pointer text-slate-500 rounded-full flex flex-row items-center justify-center hover:bg-red-300"
 
-    const {deleteTransaction} = useContext(GlobalContext)
+
+
+
   return (
-    
-        <li className='TransactionList hover:drop-shadow-2xl' data-type={typeof transaction.amount} >
-                <span onClick={() => deleteTransaction(transaction.id)} className={trash}>close</span>
-                <div>
-                <span className="material-symbols-rounded text-blue-500 bg-blue-300 w-10 h-10 rounded-full flex flex-rox items-center justify-center"> {transaction.amount < 0 ? 'outBox':'move_to_inbox'}</span>
-                </div>
-                <div className='transaction'>
-                    <span className=' max-w-10 text-xl font-semibold hover:text-blue-500 truncate'>{transaction.text}</span>
-                    <span className=' text-sm font-light'>11/12/2023</span>
-                </div>
+        <>
+            <div className={'TransactionBox hover:drop-shadow-2xl ' + className}>
+                {!!budget && <p>{budget.category}</p>}
+                <li className='TransactionList' >
+                        {!closeButtonHidden && <span onClick={onDeleteButtonClick} className={trash}>close</span>}
+                        <div>
+                        <span className="material-symbols-rounded text-blue-500 bg-blue-300 w-10 h-10 rounded-full flex flex-rox items-center justify-center"> {transaction.amount < 0 ? 'outBox':'move_to_inbox'}</span>
+                        </div>
+                        <div className='transaction'>
+                            <span className=' max-w-10 text-xl font-semibold hover:text-blue-500 truncate'>{transaction.name}</span>
+                            <span className=' text-sm font-light'>11/12/2023</span>
+                        </div>
 
-                <div className=' flex flex-row items-center justify-center space-x-1'>
-                    <span className={color}> {transaction.amount < 0 ? 'do_not_disturb_on':'add_box'}</span>
-                    <span>{sign}${Math.abs(transaction.amount)}</span>
-                </div>
-        </li>
-  )
+                        <div className=' flex flex-row items-center justify-center space-x-1'>
+                            <span className={color}> {transaction.amount < 0 ? 'do_not_disturb_on':'add_box'}</span>
+                            <span>{sign}${Math.abs(transaction.amount)}</span>
+                        </div>
+                </li>
+
+            </div>
+        </>
+         )
 }
+
+
+export default Transaction;
