9b949afceeadb5aa3ad5c113f686a00b7b75daf6
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-23 01:24:19 +0100

    Addin actions to our reducer

--- src/Context/AppReducer.jsx
+++ src/Context/AppReducer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default (state, action) => {
 
+
     switch(action.type){
+
         case 'DELETE_TRANSACTION':
             return{
                 ...state,
@@ -12,19 +14,25 @@ export default (state, action) => {
         case 'DELETE_BUDGET':
             return{
                 ...state,
-                budgets: state.budgets.filter(budget => budget.id !== action.payload)
+                budgets: state.budgets.filter(budget => budget.id !== action.payload),
+                transactions: state.transactions.filter(transaction => transaction.budgetId !== action.payload),
             }    
         case 'ADD_TRANSACTION':
             return{
                 ...state,
                 transactions: [action.payload, ...state.transactions]
             }    
-
+        case 'ADD_BUDGET':
+            return{
+                ...state,
+                budgets: [action.payload, ...state.budgets]
+            }    
+    
         default:
             return state;
     }
 
-
+ 
 
   
 }
