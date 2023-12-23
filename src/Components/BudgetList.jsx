45718b3f0b53553061309de8ea5e38aaee8c111a
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-23 01:02:56 +0100

    budgetList main component that mapping on the Budget components

--- src/Components/BudgetList.jsx
+++ src/Components/BudgetList.jsx
@@ -5,11 +5,26 @@ import { Budget } from './Budget'
 
 export default function BudgetList() {
 
-    const { budgets } = useContext(GlobalContext)
+    const { 
+      budgets, 
+      getTotalTransaction, 
+      deleteBudget,
+      getTransactionsByBudgetId,
 
+    } = useContext(GlobalContext);
+
+    
   return (
-    <ul>
-        {budgets.map(budget => (<Budget key={budget.id} budget={budget} />))}
+    <ul className="flex flex-col w-full h-auto items-center justify-start py-8 px-0 lg:px-8">
+        {budgets.map(budget => (
+          <Budget 
+            key={budget.id} 
+            budget={budget} 
+            transactions={getTransactionsByBudgetId(budget.id)}
+            totalTransaction={getTotalTransaction(budget.id)}
+            onDeleteButtonClick={() => (prompt('Enter "yes" to delete this budget and all transactions') === "yes") && deleteBudget(budget.id)}
+          />
+        ))}
     </ul>
   )
 }
