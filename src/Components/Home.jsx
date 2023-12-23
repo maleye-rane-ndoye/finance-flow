3cb6640328d46582d13ef2214f685b547e4c6ed0
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-23 01:18:22 +0100

    import the chart in the home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Params from './Params'
 import IncomExpenses from './IncomExpenses'
 import BudgetList from './BudgetList'
+
 export default function Home() {
+
+
+  
   return (
     <>
       <Params/>
       <div className='Home'>
        <IncomExpenses/>
-       <BudgetList/>
+       <div className='w-full h-full overflow-auto'>
+            <BudgetList/>
+       </div>
       </div>
     </>
   )
