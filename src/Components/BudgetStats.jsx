bd2c2b67eacc034eed54b3b215b0369daa4dcee4
maleye rane <maleyeranendoye@Mydebian.lan>
2023-12-23 01:12:02 +0100

    budgetStats for the chart summary all statistiques transactions and budgets flow

--- src/Components/BudgetStats.jsx
+++ src/Components/BudgetStats.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Params from './Params'
-export default function BudgetStats() {
+import { GlobalContext } from '../Context/GlobalStats'
+import Graph from './Graph.jsx'
+import GraphTransactions from './GraphTransactions.jsx'
+
+
+export default function BudgetStats(props) {
+
+  const { getCurrentGraphBudget,getCurrentGraphTransaction } = useContext(GlobalContext);
+
+  const data = getCurrentGraphBudget()
+  const data2 = getCurrentGraphTransaction()
+
+
   return (
     <>
         <Params/>
         <div className='BudgetStats'>
-            his is the budget stats
+          <h1 className=' text-lg my-4 font-bold'>Summary </h1>
+             <Graph data={data}/>
+          <h2 className=' text-lg my-4 underline'>Budgets chart summary</h2>
+
+
+             <GraphTransactions data1={data} data2={data2}/>
+          <h2 className=' text-lg my-4 underline'>Transactions chart summary</h2>
         </div>
     </>
   )
